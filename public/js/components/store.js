b9a6555d187d9dd4d523feb027e11a77737650f5
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-07-02 20:55:51 -0400

    set up heroku files

--- public/js/components/store.js
+++ public/js/components/store.js
@@ -1,45 +1,49 @@
 "use strict";
 const myStore = {
   template: `
-  <div class="sectionHeader">
+  <div class="sectionHeader" id="storeHeader">
     <a href="#!/cartlist"><i class="fas fa-shopping-cart"> View Cart</i> </a>
+    <p class="total" >Grand Total: $$</p>
   </div>
-<section class="store">
   
+  <section class="store">
+    
 
-  <div class="item" ng-repeat="item in $ctrl.store">
-    <div class="store__product">
-      <div class="item__line">
-          <h4>{{item.product}}</h4>
-          <p>{{item.price}}</p>
+    <div class="item" ng-repeat="item in $ctrl.store">
+      <div class="store__product">
+        <div class="item__line">
+            <h4>{{item.product}}</h4>
+            <p>{{item.price}}</p>
+        </div>
+        <img class="img_med" src={{item.url}}>
       </div>
-      <img class="img_med" src={{item.url}}>
+        <div class="store__product_add">
+          <i class="fas fa-shopping-bag"></i>
+          <p ng-click="$ctrl.addToCart(item, $event);">Add to Cart</p>
+        
+        </div>
     </div>
-    <p ng-click="$ctrl.addToCart(item); $ctrl.changeColor($event);">
-      <i class="fas fa-plus"></i>
-    </p>
-  
-  </div>
-
-</section>
+  </section>
     
   `,
 
-  controller: ["StoreService", function(StoreService){
+  controller: ["StoreService", "$timeout", function(StoreService, $timeout){
     const vm = this;
+ 
 
     StoreService.getStoreItems().then((response)=>{
       vm.store = response.data;
       console.log(vm.store);
-      
-      vm.addToCart = (item) => {
-        console.log("addtocartworking");
-      
-        StoreService.addToCart(item).then((response) => {
-          console.log(response);
-          });
-      };
+   
     });
+
+    vm.addToCart = (item, $event) => {
+      console.log($event.path[1]);      
+      
+      StoreService.addToCart(item).then((response) => {
+        console.log(response);
+        });
+    };
   }]
 };
 
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use("/portal", store);
 
 
 //set up port, 3000 or 8080, my choice!
-let port = 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => { 
   console.log(`Server listening on ${port}.`);

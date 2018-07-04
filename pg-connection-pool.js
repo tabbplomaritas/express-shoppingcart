8c557b531dc621328d2b895eafbe65c0e09d32d0
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-07-03 20:53:17 -0400

    setting up heroku database

--- pg-connection-pool.js
+++ pg-connection-pool.js
@@ -1,17 +1,34 @@
 "use strict";
 
-//step one: require pg.
+// const pg = require("pg");
+
+
+// const pool = new pg.Pool({
+//   user: "postgres", 
+//   password: "plomaritasgc", 
+//   host: "localhost",
+//   port: 5432, 
+//   database: "ExpressShopDB",
+//   ssl: false
+// });
+
+
+// module.exports = pool;
+
 const pg = require("pg");
+const url = require("url");
+try {
+require("dotenv").config();
+}
 
-//step two: create pool
-const pool = new pg.Pool({
-  user: "postgres", 
-  password: "plomaritasgc", 
-  host: "localhost",
-  port: 5432, 
-  database: "ExpressShopDB",
-  ssl: false
-});
-
-
-module.exports = pool;
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+const config = {
+user: auth[0],
+password: auth[1],
+host: params.hostname,
+port: params.port,
+database: params.pathname.split("/")[1],
+ssl: params.hostname !== "localhost"
+};
+module.exports = new pg.Pool(config);

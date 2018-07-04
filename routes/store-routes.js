7b02d8187284615a09c3f44c76486badf1ea971f
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-07-04 17:49:42 -0400

    created new shopping_cart table in DB, updated store-routes to send new columns

--- routes/store-routes.js
+++ routes/store-routes.js
@@ -14,7 +14,7 @@ storeRouter.get("/store", (req, res) => {
 });
 
 storeRouter.post("/store", (req, res) => {
-  pool.query("INSERT INTO shopping_cart (product, price, quantity, url) VALUES ($1::text, $2::money, $3::int, $4::text)", [req.body.product, req.body.price, 1, req.body.url]).then(() => {
+  pool.query("INSERT INTO shopping_cart (product, price, quantity, url, category, vendor) VALUES ($1::text, $2::money, $3::int, $4::text, $5::text, $6::text)", [req.body.product, req.body.price, 1, req.body.url, req.body.category, req.body.vendor]).then(() => {
     pool.query("UPDATE shopping_cart SET item_total = quantity*price;").then(()=>{
       pool.query("SELECT * FROM store ORDER BY id").then((result) => {
       console.log(result.rows);
